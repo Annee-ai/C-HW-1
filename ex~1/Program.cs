f7ba993f6eff6e9114236db0b4e820de7504f248
Anna <qpherd@yandex.ru>
2022-12-03 13:08:17 +0300

    ex~1.0

---
+++ ex~1/Program.cs
@@ -0,0 +1,18 @@
+﻿// start 
+
+using System;
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a; 
+
+if (a < b)
+{
+    max = b;
+}
+if (max < c)
+{
+    max = c;
+}
+Console.WriteLine(max);
