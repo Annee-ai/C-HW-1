0eceb71b34c3eeb20ce932298f4e2b27eb00a51d
Anna <qpherd@yandex.ru>
2022-12-11 12:22:50 +0300

    ex09

--- Ex~09/Program.cs
+++ Ex~09/Program.cs
@@ -3,4 +3,11 @@
 //3 -> 1, 8, 27
 //5 -> 1, 8, 27, 64, 125
 //Start
-Console.Writeline("Enter the number ")
+Console.WriteLine("Enter the number ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine($"{i} x {3} = {i * 3}");
+    Console.WriteLine();
+}
+Console.ReadKey();
